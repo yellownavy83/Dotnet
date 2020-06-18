b2103b643507f98e9c83cc84b7382fbe0507430d
jinseok <yellownavy83@gmail.com>
2020-06-19 00:10:25 +0900

    Linq를 이용한 sorting 추가

---
+++ DataStructure/DataStructure.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DataStructure
+{
+    public class DataStructure
+    {
+        static List<Student> studentList = new List<Student>();
+        static void Main(string[] args)
+        {
+            studentList.Add(new Student("Tom", 17, 33.2, false)); 
+            studentList.Add(new Student("Sara", 19, 53.4, true)); 
+            studentList.Add(new Student("홍길동", 15, 130.2, false)); 
+            studentList.Add(new Student("이순신", 16, 43.2, true)); 
+            studentList.Add(new Student("Ace", 15, 23.2, false)); 
+            Console.WriteLine(Environment.NewLine + "-------------- 원본 데이터 --------------------"); 
+            foreach (var student in studentList) 
+            { 
+                Console.WriteLine(student); 
+            }
+
+            Console.WriteLine(Environment.NewLine + "-------------- Age 순 오름차순 정렬하기 --------------------"); 
+            List<Student> SortedList = studentList.OrderBy(x => x.Age).ToList(); 
+            foreach (var student in SortedList) 
+            { 
+                Console.WriteLine(student); 
+            
+            }
+            Console.WriteLine(Environment.NewLine + "------------- Name 순 내림차순 정렬하기 --------------------"); 
+            SortedList = studentList.OrderByDescending(x => x.Name).ToList(); 
+            foreach (var student in SortedList) 
+            { 
+                Console.WriteLine(student); 
+            }
+
+            Console.WriteLine(Environment.NewLine + "----------- Weight 오름차순, 반환값없는 자체 정렬하기 -----------"); 
+            studentList.Sort((x1, x2) => x1.Weight.CompareTo(x2.Weight)); 
+            foreach (var student in studentList) 
+            { 
+                Console.WriteLine(student); 
+            }
+
+            Console.WriteLine(Environment.NewLine + "----------- Weight 내림차순, 반환값없는 자체 정렬하기 -----"); 
+            studentList.Sort((x1, x2) => x2.Weight.CompareTo(x1.Weight)); 
+            foreach (var student in studentList) 
+            { 
+                Console.WriteLine(student); 
+            }
+
+            Console.WriteLine(Environment.NewLine + "----------- Age순 이후, Weight 순으로 정렬하기 -----"); 
+            SortedList = studentList.OrderBy(x => x.Age).ThenBy(x => x.Weight).ToList(); 
+            foreach (var student in SortedList) 
+            { 
+                Console.WriteLine(student); 
+            }
+
+            Console.WriteLine(Environment.NewLine + "----------- Age순 이후, Weight, name 순으로 정렬하기 -----");
+            SortedList = studentList.OrderBy(x => x.Age).ThenBy(x => x.Weight).ThenBy(x => x.Name).ToList();
+            foreach (var student in SortedList)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.Read();
+
+        }
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+        public bool Glass { get; set; } = false; // 안경끼면, true 
+        public Student(string name, int age, double weight, bool glass) 
+        { 
+            this.Name = name; 
+            this.Age = age; 
+            this.Weight = weight; 
+            this.Glass = glass; 
+        } 
+        public override string ToString() 
+        { 
+            return $"-- Name={Name},\t Age={Age}, 몸무게={Weight}, \t 안경착용={Glass}--"; 
+        } 
+    }
+
+}
---
+++ LinqSample/LinqSample.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqSample
+{
+    class LinqSample
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee("Jin", "Park", 100, true));
+            employees.Add(new Employee("Abc", "Park", 1000, false));
+            employees.Add(new Employee("Jin", "Kim", 100, true));
+            employees.Add(new Employee("Abc", "Kim", 1000, false));
+
+            List<Employee> managers = new List<Employee>();
+            foreach(Employee employee in employees)
+            {
+                if(employee.IsManager == true)
+                {
+                    managers.Add(employee);
+                    Console.WriteLine(employee);
+                }
+            }
+
+            Console.WriteLine("\n=============Linq FindAll() Test=============");
+            List<Employee> managers2 = employees.FindAll(employee => employee.IsManager == true);
+            managers2.ForEach(e => Console.WriteLine(e));
+
+            Console.WriteLine("\n=============Linq OrderBy() Test=============");
+            List<Employee> sortedList = employees.OrderBy(e => e.Salary).ToList();
+            sortedList.ForEach(e => Console.WriteLine(e));
+
+            Console.WriteLine("\n=============Linq OrderByDescending() Test=============");
+            List<Employee> sortedList2 = employees.OrderByDescending(e => e.Salary).ToList();
+            sortedList2.ForEach(e => Console.WriteLine(e));
+
+            Console.WriteLine("\n=============Linq OrderBy() ThenBy() Test=============");
+            //List<Employee> sortedList3 = employees.OrderBy(e => e.Salary).ThenBy(o => o.LastName).ToList();
+            List<Employee> sortedList3 = employees.OrderBy(e => e.Salary).ThenByDescending(o => o.LastName).ToList();
+            sortedList3.ForEach(e => Console.WriteLine(e));
+
+            Console.WriteLine("\n=============Linq Sort() Test=============");
+            employees.Sort((o1, o2) => o1.LastName.CompareTo(o2.LastName));
+            employees.ForEach(e => Console.WriteLine(e));
+
+            Console.Read();
+        }
+    }
+
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+        public bool IsManager { get; set; }
+        public Employee(string FirstName, string LastName, decimal Salary, bool IsManager)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Salary = Salary;
+            this.IsManager = IsManager;
+        }
+        public override string ToString()
+        {
+            return "FirstName: " + FirstName + " LastName: " + LastName + " Salary: " + Salary + " IsManager: " + IsManager;
+        }
+    }
+}
