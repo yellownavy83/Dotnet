8191782d01b7650a898e0b53d394313d18c658d8
jinseok <yellownavy83@gmail.com>
2020-06-22 00:15:48 +0900

    동적로딩(reflection)

---
+++ DynamicLoading/DynamicLoading.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace DynamicLoading
+{
+    class DynamicLoading
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Assembly asm = Assembly.LoadFrom(@"D:\STUDY\workspace_dotnet\Dotnet\DynamicLoadingLibrary\bin\Debug\DynamicLoadingLibrary.dll");
+            Type[] types = asm.GetExportedTypes();
+            string classFullName = string.Empty;
+            foreach(Type type in types)
+            {
+                Console.WriteLine(type.FullName);
+                classFullName = type.FullName;
+            }
+
+            //Type type = asm.GetType(classFullName);
+            dynamic obj = Activator.CreateInstance(types[0]);
+
+            int result = obj.plus(1, 2);
+            Console.WriteLine(result);
+
+            result = obj.minus(2, 3);
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ DynamicLoadingLibrary/MathLibrary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicLoadingLibrary
+{
+    public class MathLibrary
+    {
+        public int plus(int a, int b)
+        {
+            return a + b;
+        }
+        public int minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
